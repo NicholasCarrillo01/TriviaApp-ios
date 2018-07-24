8ef0a2ee325f0214a2a4b51f29134d4fb5a8c08a
Nicholas Carrillo <nicholasmcxbox@gmail.com>
2018-07-24 11:19:43 -0400

    added in property observers

--- Trivia Game Ios/view controllers/quizViewController.swift
+++ Trivia Game Ios/view controllers/quizViewController.swift
@@ -16,27 +16,56 @@ class quizViewController: UIViewController {
     @IBOutlet weak var answerButton2: UIButton!
     @IBOutlet weak var answerButton3: UIButton!
     @IBOutlet weak var answerButton4: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
     
     
+    
+    //Property observers
+    var currentQuestion: TriviaQuestion! {
+        didSet {
+            //When currentQuestion is set, update the UI with values from that question
+            questionLabel.text = currentQuestion.question
+            answerButton1.setTitle(currentQuestion.answers[0], for: .normal)
+            answerButton2.setTitle(currentQuestion.answers[1], for: .normal)
+            answerButton3.setTitle(currentQuestion.answers[2], for: .normal)
+            answerButton4.setTitle(currentQuestion.answers[3], for: .normal)
+        }
+    }
+    
+    var score = 0 {
+        didSet {
+            //update the scorelabels text whenever the score changes
+            scoreLabel.text = "\(score)"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    let newQuestion = TriviaQuestion(question: "what color is the sky", answers: ["red", "mauve", "aubregine", "blue"],    correctAnswerIndex: 3)
+    let newQuestion = TriviaQuestion(question: "what color is the sky", answers: ["red", "mauve", "aubregine", "blue"], correctAnswerIndex: 3)
     
-        questionLabel.text = newQuestion.question
-        answerButton1.setTitle(newQuestion.answers[0], for: .normal)
-        answerButton2.setTitle(newQuestion.answers[1], for: .normal)
-        answerButton3.setTitle(newQuestion.answers[2], for: .normal)
-        answerButton4.setTitle(newQuestion.answers[3], for: .normal)
-        
-        print("The correct answer is: \(newQuestion.correctanswer)")
+       currentQuestion = newQuestion
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func answerButton1(_ sender: UIButton) {
+        print(sender.tag)
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
